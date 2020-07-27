839c5192cf0ec92f605c6f77e430c2a55fd99d5d
swordcom367 <nleslie@landmarkschool.org>
2020-07-27 14:20:04 -0400

    added new test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -3,3 +3,7 @@ const add =require("../index");
 test("addes 1+2 to equle 3" , () => {
     expect(add(1,2)).toBe(3);
 });
+test("addes 1+2 to equle 3", () =>
+{
+    expect(add(1.0, 2.0)).toBe(3);
+});
